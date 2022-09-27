493969bc8245ef993710f8164e6c72fba7ec2ee0
ImaginaryHeres <114483034+ImaginaryHeres@users.noreply.github.com>
2022-09-27 20:47:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -175,5 +175,5 @@ if __name__ == "__main__":
 
     # 公众号推送消息
     for user in users:
-        send_message(user,  region, temp, note_ch, note_en)
+        send_message(user,  region, temp)
     os.system("pause")
